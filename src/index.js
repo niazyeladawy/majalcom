757ff5a8b24004de8eddc9ae4a48336c6deb4daf
niazyeladawy <niazyadawy2@gmail.com>
2021-12-27 13:21:59 +0200

    added media query file for orgainzing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import './MediaQueries.css';
 
 ReactDOM.render(
   <React.StrictMode>
